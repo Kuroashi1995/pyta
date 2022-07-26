7236c1ce79af41602482829e435f0bbd647bfc5c
kuroashi1995 <kuroashi.prog@gmail.com>
2022-07-26 15:34:04 -0400

    agregados links nuevamente

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import random
 import time
 
 auth = False
+paramedicos_determinantes_grave = ['no puede respirar', 'convulsiones', 'unhas y labios morados', 'hemorragia desangrante']
+paramedicos_grave = ['inconsciente', 'pulso rapido']
+paramedicos_intermedio = ['confusion','cefalea', 'sensibilidad a la luz', 'rigidez en el cuello', 'dolor en el hombro', 'saturacion menor a 95', 'fiebre alta', 'temperatura baja', 'dolor intenso','dolor moderado', 'hemorragia incontrolable', 'sangrado rectal', 'sangrado rectal', 'vomitos con sangre', 'golpe en la cabeza', 'golpe en el cuello', 'golpe en la espalda', 'fractura abierta']
+paramedicos_leve = ['fiebre', 'lesiones leves', 'infeccion', 'perdida aguda de audicion', 'entumecimiento']
 
 def generar_pin():
     pin = int(random.randint(1000,10000))
@@ -56,3 +60,34 @@ def refrescar_pin():
         socketio.emit('new pin', pin)
         print(pin)
         time.sleep(30)
+
+control = ['no puede respirar', 'inconsciente', 'pulso rapido', 'cefalea']
+
+def triage_paramedicos(datos):
+    contador_det_grave = 0
+    contador_grave = 0
+    contador_intermedio = 0
+    contador_leve = 0
+    for dato in datos:
+        if dato in paramedicos_determinantes_grave:
+            contador_det_grave += 1
+        elif dato in paramedicos_grave:
+            contador_grave += 1
+        elif dato in paramedicos_intermedio:
+            contador_intermedio += 1
+        elif dato in paramedicos_leve: 
+            contador_leve += 1
+    
+    if contador_det_grave > 0 or contador_grave > 1:
+        return('rojo')
+    elif contador_grave > 0 and contador_intermedio > 2:
+        return('rojo')
+    elif contador_intermedio > 2:
+        return('naranja')
+    elif contador_intermedio > 1 and contador_leve > 2:
+        return('naranja')
+    else:
+        return('verde')
+
+
+print(triage_paramedicos(control))
